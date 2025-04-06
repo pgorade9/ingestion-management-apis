548bd31b82deb2a2b17729d49a4cfbc9b718fd35
pgorade <pgorade@slb.com>
2025-04-06 17:44:38 +0530

    code refactor

--- apis/entitlements.py
+++ apis/entitlements.py
@@ -1,3 +1,5 @@
+from typing import Literal
+
 from fastapi import APIRouter, Query
 from services import entitlements_service
 
@@ -5,18 +7,22 @@ from configuration import keyvault
 
 ent_router = APIRouter(prefix="/api/entitlements/v2", tags=["entitlement apis"])
 
+env_list = [key for key in keyvault.keys() if
+            isinstance(keyvault[key], dict) and keyvault[key].get("data_partition_id") is not None]
+
+data_partition_list = list({keyvault[key].get("data_partition_id") for key in keyvault.keys() if
+                            isinstance(keyvault[key], dict) and keyvault[key].get("data_partition_id") is not None})
+
 
 @ent_router.get("/groups")
-def get_groups(env: str = Query(None, description="Environment",
-                                enum=keyvault["envs-ltops"] + keyvault["envs"])):
+def get_groups(env: Literal[*env_list] = Query(...)):
     return entitlements_service.get_groups(env)
 
 
 @ent_router.get("/members/{user}/groups",
                 description="data.default.owners@domain, pgorade@slb.com, users.datalake.admins@domain")
 def get_members_groups(user: str = "user.datalake.admins@domain",
-                       env: str = Query(None, description="Environment",
-                                        enum=keyvault["envs-ltops"] + keyvault["envs"]),
+                       env: Literal[*env_list] = Query(...),
                        member_type: str = Query("DATA", enum=["USER", "DATA", "SERVICE"])
                        ):
     return entitlements_service.get_members_groups(user, env, member_type)
