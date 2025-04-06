cac1a055a71732674e72ad0a59d81462a7140431
pgorade <pgorade@slb.com>
2025-04-06 17:47:37 +0530

    code refactor

--- apis/legal_tags.py
+++ apis/legal_tags.py
@@ -1,3 +1,5 @@
+from typing import Literal
+
 from fastapi import APIRouter, Query
 from services import legal_tag_service
 
@@ -5,8 +7,9 @@ from configuration import keyvault
 
 legal_router = APIRouter(prefix="/api/legal/v1", tags=["legal tag apis"])
 
+env_list = [key for key in keyvault.keys() if
+            isinstance(keyvault[key], dict) and keyvault[key].get("data_partition_id") is not None]
 
 @legal_router.get("/legaltags")
-def get_groups(env: str = Query(None, description="Environment",
-                                enum=keyvault["envs-ltops"] + keyvault["envs"])):
+def get_groups(env: Literal[*env_list] = Query(...)):
     return legal_tag_service.get_legal_tags(env)
--- apis/workflow.py
+++ apis/workflow.py
@@ -1,3 +1,5 @@
+from typing import Literal
+
 from fastapi import APIRouter, Query
 from services import workflow_service
 
@@ -5,16 +7,17 @@ from configuration import keyvault
 
 workflow_router = APIRouter(prefix="/api/workflow/v1", tags=["workflow apis"])
 
+env_list = [key for key in keyvault.keys() if
+            isinstance(keyvault[key], dict) and keyvault[key].get("data_partition_id") is not None]
+
 
 @workflow_router.get("/workflow")
-def get_workflows(env: str = Query(None, description="Environment",
-                                   enum=keyvault["envs-ltops"] + keyvault["envs"])):
+def get_workflows(env: Literal[*env_list] = Query(...)):
     return workflow_service.get_workflows(env)
 
 
 @workflow_router.get("/workflow/{workflow_name}")
-def get_workflow(env: str = Query(None, description="Environment",
-                                  enum=keyvault["envs-ltops"] + keyvault["envs"]),
+def get_workflow(env: Literal[*env_list] = Query(...),
                  dag: str = Query(None, enum=["csv_parser_wf_status_gsm",
                                               "wellbore_ingestion_wf_gsm",
                                               "doc_ingestor_azure_ocr_wf",
@@ -24,8 +27,7 @@ def get_workflow(env: str = Query(None, description="Environment",
 
 
 @workflow_router.post("/workflow}")
-def register_workflow(env: str = Query(None, description="Environment",
-                                       enum=keyvault["envs-ltops"] + keyvault["envs"]),
+def register_workflow(env: Literal[*env_list] = Query(...),
                       dag: str = Query(None, enum=["csv_parser_wf_status_gsm",
                                                    "wellbore_ingestion_wf_gsm",
                                                    "doc_ingestor_azure_ocr_wf",
